a69494bc133d493f791f6b267a1dd2f1ab532c58
Victor Ogatha <116044128+Vtogatha@users.noreply.github.com>
2022-11-22 21:25:53 -0300

    Add files via upload

---
+++ OperacoesAritmeticas.py
@@ -0,0 +1,13 @@
+# Faca um algoritmo para somar dois numeros e multiplicar o resultado pelo primeiro numero.
+
+numero1 = int(input("Informe o primeiro número:"))
+numero2 = int(input("Informe o segundo numero:"))
+
+soma = numero1 + numero2
+mult = soma * numero1
+
+print("o resultado da multiplicacao e {0}".format(mult))
+
+
+
+
